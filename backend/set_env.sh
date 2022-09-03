2b1ab239e2816c891b863af48b2263b18dc00c8a
Pierre Malak <Pierre_malak@hotmail.com>
2022-09-03 03:41:18 +0200

    New Project

--- backend/set_env.sh
+++ backend/set_env.sh
@@ -9,3 +9,17 @@ export TYPEORM_PORT=5532
 export TYPEORM_USERNAME=postgres
 export TYPEORM_PASSWORD=password
 export TYPEORM_DATABASE=glee
+
+
+
+export NODE_ENV = local
+export VERSION = 1
+export TYPEORM_CONNECTION = postgres
+export TYPEORM_MIGRATIONS_DIR= ./src/migrations
+export TYPEORM_ENTITIES = ./src/modules/domain/**/*.entity{.ts,.js}
+export TYPEORM_MIGRATIONS = ./src/migrations/*{.ts,.js}
+export TYPEORM_HOST = localhost
+export TYPEORM_PORT = 5532
+export TYPEORM_USERNAME = postgres
+export TYPEORM_PASSWORD = password
+export TYPEORM_DATABASE = glee
